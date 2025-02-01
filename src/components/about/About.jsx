c58126c07d4ae20b40dbd93f85a15b0dfc3d60ee
barlakshan <ravindu.ba.lakshan@gmail.com>
2025-02-01 23:02:11 +0530

    change font

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -70,7 +70,7 @@ const About = () => {
             initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1.5, delay: 0.5 }}
-            className="text-2xl font-sriracha text-amber-900"
+            className="text-2xl font-pacifico text-amber-900"
           >
             ☕ Ceylon Brew – Experience Authenticity in Every Sip
           </motion.h1>
